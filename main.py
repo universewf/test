af64fadde9378074c4ae8ca9f25aa8e592951963
Garik Baghdasaryan <mcclean449@gmail.com>
2025-02-23 21:20:34 +0300

    one

---
+++ main.py
@@ -0,0 +1,10 @@
+first = 'Audi'
+second = 'Bmw'
+third = 'Mercedes'
+
+
+print(f"{first} is symbios between strict and practicality")
+
+print(f"{second} its all about speed and controllability ")
+
+print(f"{third} it's all about comfort")
